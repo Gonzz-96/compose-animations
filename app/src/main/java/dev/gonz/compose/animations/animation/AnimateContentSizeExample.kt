7a4662eb938030926de173189a43b10121e9a879
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-20 18:19:21 -0600

    Create AbunateContentSize example.

---
+++ app/src/main/java/dev/gonz/compose/animations/animation/AnimateContentSizeExample.kt
@@ -0,0 +1,55 @@
+package dev.gonz.compose.animations.animation
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.gonz.compose.animations.ui.theme.ComposeAnimationsTheme
+
+@Composable
+fun AnimateContentSizeExample(
+    modifier: Modifier = Modifier
+) {
+    var size by remember { mutableStateOf(Size(100F, 100F)) }
+
+    Surface(modifier = modifier) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Animate Content Size",
+                fontSize = 32.sp,
+                modifier = Modifier
+                    .clickable {
+                        size = if (size.height == 100F)
+                            Size(200F, 200F)
+                        else
+                            Size(100F, 100F)
+                    }
+                    .padding(vertical = 16.dp))
+            Box(
+                modifier = Modifier
+                    .animateContentSize()
+                    .size(size.height.dp)
+                    .background(Color.Red))
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun Preview_AnimateContentSizeExample() {
+    ComposeAnimationsTheme {
+        AnimateContentSizeExample()
+    }
+}
