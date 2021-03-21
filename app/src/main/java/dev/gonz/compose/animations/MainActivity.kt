9d37116e4bd5115a78bfc105a686f0ee6e414ca3
Gonzalo Campos <erick.campos@wizeline.com>
2021-03-20 18:12:55 -0600

    Rearrange MainActivity.

--- app/src/main/java/dev/gonz/compose/animations/MainActivity.kt
+++ app/src/main/java/dev/gonz/compose/animations/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import dev.gonz.compose.animations.animation.AnimateContentSizeExample
 import dev.gonz.compose.animations.animation.AnimatedVisibilityExample
 import dev.gonz.compose.animations.ui.theme.ComposeAnimationsTheme
 
@@ -24,12 +26,22 @@ class MainActivity : ComponentActivity() {
             ComposeAnimationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        AnimatedVisibilityExample(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 20.dp))
-                        Divider()
+                    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+                        item {
+                            AnimatedVisibilityExample(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 20.dp))
+                        }
+                        item { Divider() }
+                        item {
+                            AnimateContentSizeExample(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(vertical = 20.dp)
+                            )
+                        }
+                        item { Divider() }
                     }
                 }
             }
